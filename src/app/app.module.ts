1e89e515167fa9cfe19a29eef182a351daf18465
Max <torabora851@gmail.com>
2017-10-28 12:34:55 +0300

    Добавил материал

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule }          from '@angular/forms';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 import { DynamicFormFieldComponent } from './components/dynamic-form-field/dynamic-form-field.component';
 import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +18,12 @@ import { FooterComponent } from './components/footer/footer.component';
     DynamicFormFieldComponent 
   ],
   imports: [
-    BrowserModule, ReactiveFormsModule   
+    BrowserModule, 
+    ReactiveFormsModule, 
+    BrowserAnimationsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
