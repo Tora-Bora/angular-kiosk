f46949b4c69a4672656ae0b4d7f8424669901330
Max <torabora851@gmail.com>
2017-10-29 12:23:38 +0300

    Подключил jquery virtual_keyboard

--- src/app/components/dynamic-form-field/dynamic-form-field.component.ts
+++ src/app/components/dynamic-form-field/dynamic-form-field.component.ts
@@ -1,19 +1,49 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { KeyboardOptions, NavigateOptions } from "virtual-keyboard";
+import { AfterViewInit } from '@angular/core';
+
+import * as $ from "jquery"; 
+import "virtual-keyboard";
 
 import { FieldBase } from '../../models/fields/field-base';
 
+const kbOptions: KeyboardOptions = {
+  display: {
+      bksp: "\u2190",
+      accept: `Next`,
+      cancel: `Back`,
+      normal: "ABC",
+      meta1: "#+-",
+      space: "Space",
+      alt: `Alt`,
+      s: `ABC`,
+  },
+  acceptValid: true,
+  type: "input",
+  layout       : 'num', 
+  lockInput: true,
+  alwaysOpen: true,
+  appendLocally: true,
+  color: "light",
+  class: "sxcycx",
+  updateOnChange: true,
+  usePreview: false,
+  tabNavigation: false,
+  canceled: () => { console.log("cancelled"); }
+};
 
 @Component({
   selector: 'df-field',
   templateUrl: './dynamic-form-field.component.html',
   styleUrls: ['./dynamic-form-field.component.css']
 })
-export class DynamicFormFieldComponent implements OnInit {
+export class DynamicFormFieldComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    $('input').keyboard(kbOptions);
   }
 
   @Input() field: FieldBase<any>;
